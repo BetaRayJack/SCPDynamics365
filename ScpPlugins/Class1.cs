b5a86590939e4dec1cb8ec19e867359feb04d689
Jose Manuel Ferreira Suárez <josemanuel.ferreira@arbentia.com>
2026-06-17 11:06:27 +0200

    Test

---
+++ ScpPlugins/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScpPlugins
+{
+    public class Class1
+    {
+    }
+}
